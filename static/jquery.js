3fa5e92dc460eb87696a68d84f7a5869aaa24eb2
Samuel <201501502@rajalakshmi.edu.in>
2023-01-18 18:13:40 +0530

    Design Changes with day Update

--- static/jquery.js
+++ static/jquery.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var $cards = $('.card');
     var currentCard = 0;
+    var today = new Date().getDay();
     
     function showCard() {
       $cards.eq(currentCard).fadeIn(1000);
@@ -11,4 +12,23 @@ $(document).ready(function() {
     }
     
     setInterval(showCard, 1000);
+
+    console.log("value of day: "+today);
+
+    if(today==2){
+      $cards.eq(0).addClass('highlight');
+    }
+    else if(today==3){
+      $cards.eq(1).addClass('highlight');
+    }
+    else if(today==4){
+      $cards.eq(2).addClass('highlight');
+    }
+    else if(today==5){
+      $cards.eq(3).addClass('highlight');
+    }
+    else if(today==6){
+      $cards.eq(4).addClass('highlight');
+    }
+
   });
